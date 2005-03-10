4601006677655284a5b54b7e198ebd0757d6267f
Marius Kintel <kintel@sim.no>
2005-03-10 16:10:04 +0000

    SoQt port of viewer.cpp

---
+++ apps/soqtviewer.cpp
@@ -0,0 +1,73 @@
+
+#include <assert.h>
+#include <stdio.h>
+
+#include <Inventor/SoDB.h>
+#include <Inventor/nodes/SoSeparator.h>
+#include <Inventor/nodes/SoRotor.h>
+#include <Inventor/nodes/SoCube.h>
+#include <Inventor/nodes/SoDirectionalLight.h>
+#include <Inventor/nodes/SoPerspectiveCamera.h>
+
+#include <Inventor/Qt/SoQt.h>
+#include <Inventor/Qt/SoQtRenderArea.h>
+
+#include <NutsnBolts/NutsnBolts.h>
+#include <NutsnBolts/NbSceneManager.h>
+#include <NutsnBolts/navigation/NbNavigationMode.h>
+#include <NutsnBolts/navigation/NbNavigationSystem.h>
+
+// *************************************************************************
+
+void
+modechange(void * closure, NbNavigationSystem * system)
+{
+  fprintf(stderr, "mode change: %s\n",
+    system->getCurrentModeName().getString());
+}
+
+int
+main(int argc, char ** argv)
+{
+  QWidget *window = SoQt::init("test", "test");
+  NutsnBolts::init();
+
+  SoQtRenderArea * ra = new SoQtRenderArea(window);
+  ra->show();
+  SoQt::show(window);
+
+  NbSceneManager * sm = new NbSceneManager;
+  sm->activate();
+  ra->setSceneManager(sm);
+
+  sm->setNavigationState(NbSceneManager::JUST_NAVIGATION);
+
+  // NbNavigationSystem * nav = NbNavigationSystem::getByName(NB_EXAMINER_SYSTEM);
+  NbNavigationSystem * nav = NbNavigationSystem::getByName(NB_ZOOMER_SYSTEM);
+  nav->addModeChangeCallback(modechange, NULL);
+  sm->setNavigationSystem(nav);
+
+  SoSeparator * scene = new SoSeparator;
+  SoDirectionalLight * light = new SoDirectionalLight;
+  SoPerspectiveCamera * camera = new SoPerspectiveCamera;
+  light->direction.setValue(SbVec3f(0.5, 0.5, -1.0));
+  light->intensity.setValue(1.0);
+  SoRotor * rotor = new SoRotor;
+  rotor->speed.setValue(0.5);
+  scene->addChild(light);
+  scene->addChild(camera);
+  // scene->addChild(rotor);
+  scene->addChild(new SoCube);
+
+  camera->position.setValue(0, 0, -5);
+  camera->pointAt(SbVec3f(0, 0, 0));
+  // camera->viewAll(scene);
+
+  ra->setSceneGraph(scene);
+
+  SoQt::mainLoop();
+
+  return 0;
+}
+
+// *************************************************************************
