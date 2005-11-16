aa254c346a589285c920ab79178864208d992549
Peder Blekken <pederb@sim.no>
2005-11-16 11:59:27 +0000

    NamedEvent implementation.

--- lib/NutsnBolts/misc/NbNamedEvent.cpp
+++ lib/NutsnBolts/misc/NbNamedEvent.cpp
@@ -1,1 +1,66 @@
-// coming soon
+#include "NbNamedEvent.h"
+#include <Inventor/C/tidbits.h>
+
+// Currently used Names:
+// - MouseButtonClick:x - mouse button click event. x can be 1, 2 or 3 
+// - Tooltip - mouse button hasn't moved for some time
+// - InitSimulator - called after the scene graph has been loaded/created
+
+
+SO_EVENT_SOURCE(NbNamedEvent);
+
+static SbName * tooltip_name = NULL;
+static SbName * init_sim_name = NULL;
+
+static void namedevent_cleanup(void)
+{
+  delete tooltip_name;
+  delete init_sim_name;
+}
+
+NbNamedEvent::NbNamedEvent(void)
+  : name("")
+{
+  this->setPosition(SbVec2s(0,0));
+}
+
+NbNamedEvent::~NbNamedEvent()
+{
+}
+
+void
+NbNamedEvent::initClass(void)
+{
+  SO_EVENT_INIT_CLASS(NbNamedEvent, SoEvent);
+
+  tooltip_name = new SbName("Tooltip");
+  init_sim_name = new SbName("InitSimulator");
+
+  cc_coin_atexit((coin_atexit_f*) namedevent_cleanup);
+}
+
+void 
+NbNamedEvent::setName(const SbName & name)
+{
+  this->name = name;
+}
+
+const SbName & 
+NbNamedEvent::getName(void) const
+{
+  return this->name;
+}
+
+SbName 
+NbNamedEvent::getName(const Name name)
+{
+  switch (name) {
+  case TOOLTIP:
+    return *tooltip_name;
+  case INIT_SIMULATOR:
+    return *init_sim_name;
+  default:
+    assert(0 && "unknown name");
+  }
+  return SbName("");
+}
--- lib/NutsnBolts/misc/NbNamedEvent.h
+++ lib/NutsnBolts/misc/NbNamedEvent.h
@@ -1,1 +1,35 @@
-// coming soon
+#ifndef NB_NAMED_EVENT_H
+#define NB_NAMED_EVENT_H
+
+#include <Inventor/events/SoEvent.h>
+#include <Inventor/events/SoSubEvent.h>
+#include <Inventor/SbName.h>
+
+#include <NutsnBolts/Basic.h>
+
+class NB_DLL_API NbNamedEvent : public SoEvent {
+  typedef SoEvent inherited;
+  
+  SO_EVENT_HEADER();
+
+  NbNamedEvent(void);
+  virtual ~NbNamedEvent();
+
+  void setName(const SbName & name);
+  const SbName & getName(void) const;
+
+  static void initClass(void);
+
+  enum Name {
+    TOOLTIP,
+    INIT_SIMULATOR
+  };
+
+  static SbName getName(const Name name);
+
+ private:
+  SbName name;
+};
+
+
+#endif // NB_NAMED_EVENT_H
